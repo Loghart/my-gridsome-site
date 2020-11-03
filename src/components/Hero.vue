bf27057d626a3dfa1780e00907a3849d81ab248d
Jakub Mika <jakub.mika94@gmail.com>
2020-11-03 23:49:23 +0000

    Add some hero content

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,56 @@
 <template>
-<section class="hero">
+  <section class="hero">
     <div class="container">
-        <div class="hero">
-            <p>{{title}}</p>
-            <g-image class="hero-image" src="~/images/puzzle.jpg" width="6000"/>
+      <div class="hero-img">
+        <g-image class="hero-image" src="~/images/puzzle.jpg" width="6000" />
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <h3>Dyskrecja</h3>
+          <p>Sprawą priorytetową dla naszej firmy jest dyskrecja.</p>
         </div>
+        <div class="col-sm-4">
+          <h3>Profesjonalizm</h3>
+          <p>
+            Lada doświadczeń w kryminalistyce sprawiają, że lece ostro bez
+            trzymanki elo.
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Zaangażowanie</h3>
+          <p>
+            Każda sprawa jest dla nas priorytetowa. Wkładamy ogrom sił w
+            ustaleniu prawdy.
+          </p>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <h1>Krzysztof Mika</h1>
+          </div>
+          <div class="col-sm-9 dupa">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <p>
+              Każda sprawa jest dla nas priorytetowa. Wkładamy ogrom sił w
+              ustaleniu prawdy.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-</section>
+  </section>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style lang="scss">
 @import "@/assets/hero.scss";
