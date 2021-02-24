eab1a28afd855dea9c7fbc769966f79e5a9a59ba
Jakub Mika <jakub.mika94@gmail.com>
2021-02-24 05:19:59 +0000

    Add some content on offer page

--- src/pages/Jp2gmd.vue
+++ src/pages/Jp2gmd.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
     <Layout />
+    <div class="container">
+      <div class="col-md-12 offer-text">
+        <h1>OFERTA INDYWIDUALNA</h1>
+      </div>
+      <div class="col-md-12 offer-section-text">
+        <h2>OCHRONA WIERZYCIELI</h2>
+      </div>
+      <div class="offer-boxes">
+        <div class="row">
+          <div class="col-md-3 box">
+            <p>Weryfikacja posiadanych przez wierzyciela informacji o dłużniki oraz określenie możliwości spłaty zadłużenia.</p>
+          </div>
+          <div class="col-md-3 box">
+            <p>Dochodzenie roszczeń finansowych, poszukiwanie spadkobierców, dłużników uchylających sie od spłat pożyczek, kredytów, alimentów.</p>
+          </div>
+          <div class="col-md-3 box">
+            <p>Działania, z pełnomocnictwa wierzyciela, mające na celu skłonienie dłużnika do dobrowolnej spłaty długu w krótkim terminie lub w porozumieniu ze zleceniodawcą określenie sposobu i terminu spłaty zobowiązania.</p>
+          </div>
+          <div class="col-md-3 box">
+            <p>Nadzorowanie terminowości spłaty, w tym również spłat poszczególnych rat - ustalonych porozumieniem pomiędzy dłużnikiem, a wierzycielem. Prowadzenie działań sądowych.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 offer-section-text">
+        <h2>SPRAWY CYWILNE</h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,3 +38,8 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import "@/assets/iOffer.scss";
+</style>
+
